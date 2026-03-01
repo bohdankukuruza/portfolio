61fc40d9a11a9b1aa0255143a050d1fad650f8ea
Bohdan <bohdan.kukuruza2@mail.dcu.ie>
2026-03-01 03:19:45 +0000

    Add Toaster component and ContactSection; integrate into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -21,12 +22,11 @@ export const Home = () => {
 
         {/* Main Content */}
         <main>
-
         <HeroSection />
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
-
+        <ContactSection />
         </main>
 
         {/* Footer */}
