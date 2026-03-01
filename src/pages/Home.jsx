e71440fdc1b9cf9ddd9a6dbcb96386d84b0bb742
Bohdan <bohdan.kukuruza2@mail.dcu.ie>
2026-03-01 02:54:37 +0000

    Add ProjectsSection component and integrate it into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { StarBackground } from "../components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -24,6 +25,7 @@ export const Home = () => {
         <HeroSection />
         <AboutSection />
         <SkillsSection />
+        <ProjectsSection />
 
         </main>
 
